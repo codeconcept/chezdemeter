cf86dd8d9fc2864c0a66119163d00ff6dd1141f3
Code Concept <contact@codeconcept.com>
2024-02-23 16:15:49 +0100

    Retrieve a query parameter from a parameter

--- src/app/pages/evenings/evenings.component.ts
+++ src/app/pages/evenings/evenings.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-evenings',
   standalone: true,
   imports: [],
   templateUrl: './evenings.component.html',
-  styleUrl: './evenings.component.css'
+  styleUrl: './evenings.component.css',
 })
 export class EveningsComponent {
+  place: string = '';
 
+  @Input()
+  set town(townName: string) {
+    console.log({ townName });
+    this.place = townName;
+
+    if (townName === undefined) {
+      this.place = 'Rennes et Nantes';
+    }
+  }
 }
