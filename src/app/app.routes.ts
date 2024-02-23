b2c59d44317cea98b8e16820f7cde4f124ca8799
Code Concept <contact@codeconcept.com>
2024-02-23 16:05:11 +0100

    Add a query parameter to a url

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { MenuComponent } from './pages/menu/menu.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { EveningsComponent } from './pages/evenings/evenings.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'menu', component: MenuComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'soirees', component: EveningsComponent}
 ];
---
+++ src/app/pages/evenings/evenings.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-evenings',
+  standalone: true,
+  imports: [],
+  templateUrl: './evenings.component.html',
+  styleUrl: './evenings.component.css'
+})
+export class EveningsComponent {
+
+}
