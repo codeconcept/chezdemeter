1d5b3e5500f05b9db8e704db9fb840fe0b04d280
Code Concept <contact@codeconcept.com>
2024-03-03 20:23:49 +0100

    Retrieve route params

--- src/app/pages/menudetails/menudetails.component.ts
+++ src/app/pages/menudetails/menudetails.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-menudetails',
   standalone: true,
   imports: [],
   templateUrl: './menudetails.component.html',
-  styleUrl: './menudetails.component.css'
+  styleUrl: './menudetails.component.css',
 })
-export class MenudetailsComponent {
+export class MenudetailsComponent implements OnInit {
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    console.log(id);
+    const foodType = this.route.queryParams
+      .pipe(tap((data) => console.log({ data })))
+      .subscribe();
+  }
 
+  route = inject(ActivatedRoute);
 }
