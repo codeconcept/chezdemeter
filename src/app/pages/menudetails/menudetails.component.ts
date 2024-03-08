489162e250ae3748c8b8e8ea636ec2bbe1d4d63e
Code Concept <contact@codeconcept.com>
2024-03-08 18:46:09 +0100

    Currency pipe with standalone components

--- src/app/pages/menudetails/menudetails.component.ts
+++ src/app/pages/menudetails/menudetails.component.ts
@@ -1,17 +1,23 @@
 import { Component, OnInit, inject, signal } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { switchMap, tap } from 'rxjs';
-import { MenuService } from '../../services/menu.service';
+import { FoodItem, MenuService } from '../../services/menu.service';
+import { CurrencyPipe } from '@angular/common';
+
+import { registerLocaleData } from '@angular/common';
+import fr from '@angular/common/locales/fr';
 
 @Component({
   selector: 'app-menudetails',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './menudetails.component.html',
   styleUrl: './menudetails.component.css',
 })
 export class MenudetailsComponent implements OnInit {
   ngOnInit(): void {
+    registerLocaleData(fr, 'fr');
+
     const id = this.route.snapshot.paramMap.get('id');
     console.log(id);
     const foodType = this.route.queryParams
@@ -26,10 +32,12 @@ export class MenudetailsComponent implements OnInit {
       .subscribe({
         next: (res) => {
           console.log({ insideNExt: res });
+          this.menuItemSig.set(res);
         },
       });
   }
 
   route = inject(ActivatedRoute);
   menuService = inject(MenuService);
+  menuItemSig = signal<FoodItem>({} as FoodItem);
 }
